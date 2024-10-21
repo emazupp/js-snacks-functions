35e0f0bb0bcc3bd6c87c2730289c33047f7d9a64
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-21 14:56:31 +0200

    iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,17 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+const initialLetterArray = (array, newArray = []) => {
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    newArray[i] = currentElement[0];
+  }
+  return newArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initialLetterArray(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
