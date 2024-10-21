b25050f9443c66775e28b5190f99fc5c2e5720cc
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-21 15:10:23 +0200

    saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,11 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
-
+const saluta = (name) => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluta(name));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
